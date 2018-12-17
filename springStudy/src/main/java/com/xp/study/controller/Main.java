d8d05ea2c040ec365ed341d0267860fe4ab670a9
xpengStudy <xpengStudy@126.com>
2018-12-17 17:31:24 +0800

    spring 手动装配 集合属性

--- springStudy/src/main/java/com/xp/study/controller/Main.java
+++ springStudy/src/main/java/com/xp/study/controller/Main.java
@@ -5,6 +5,11 @@ import com.xp.study.service.PersonService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -22,6 +27,36 @@ public class Main {
         ApplicationContext ctx  = new ClassPathXmlApplicationContext("META-INFO/applicationContext.xml");
         PersonService personService = (PersonService)ctx.getBean("personServiceDi");
         personService.save();
+        //获取list对象
+        System.out.println("/**********************  list ****************/");
+        List<String> lists = personService.getList();
+        for(String string:lists){
+            System.out.println(string);
+        }
+        System.out.println("/**********************  Set ****************/");
+        //获取set对象
+        Set<String>  sets = personService.getSet();
+        for(String string:sets){
+            System.out.println(string);
+        }
+        System.out.println("/**********************  Properties ****************/");
+        //获取property对象
+        Properties properties = personService.getPro();
+        for(String string:properties.stringPropertyNames()){
+            System.out.println(string);
+        }
+
+        System.out.println("/**********************  map ****************/");
+
+
+        //获取map对象
+        Map<String,String> maps = personService.getMap();
+        for(String string:maps.values()){
+            System.out.println(string);
+        }
+
+
+
 
     }
 }
